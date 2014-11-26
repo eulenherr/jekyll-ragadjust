8859e1efcc00d5b4ec383b7c6b5b73209de632ff
eulenherr <sebastian@eulenherr.de>
2014-11-26 11:21:58 +0100

    place dashes at the end of a line, not in front

--- ragadjust.rb
+++ ragadjust.rb
@@ -3,7 +3,7 @@ module Jekyll
 		def ragadjust(text, method = 'all')
 			preps = /(\s|^|>)((aboard|about|above|across|after|against|along|amid|among|anti|around|before|behind|below|beneath|beside|besides|between|beyond|concerning|considering|despite|down|during|except|excepting|excluding|following|from|inside|into|like|minus|near|onto|opposite|outside|over|past|plus|regarding|round|save|since|than|that|this|through|toward|towards|under|underneath|unlike|until|upon|versus|with|within|without)\s)+/i
 			smallwords = /(\s|^)(([a-zA-Z-_(]{1,2}('|’)*[a-zA-Z-_,;]{0,1}?\s)+)/i # words with 3 or less characters
-			dashes = /([-–—])\s/i
+			dashes = /\s([-–—])/i
 			emphasis = /(<(strong|em|b|i)>)(([^\s]+\s*){2,3})?(<\/(strong|em|b|i)>)/i
 			numbers = /([0-9])+\s/
 
